6e2476997982d0566b0404392ebf7885b906db47
taisei0828 <o.taisei.0828@gmail.com>
2026-01-31 12:34:59 +0900

    Update news.js

--- js/news.js
+++ js/news.js
@@ -1,1 +1,27 @@
+const newsData = [
+  {
+    date: "2026.1.14",
+    title: "コンサート 更新",
+    url: "#"
+  },
+  {
+    date: "2026.1.11",
+    title: "Special 更新",
+    url: "#"
+  },
+  {
+    date: "2026.1.1",
+    title: "お知らせ",
+    url: "#"
+  }
+];
+
+const list = document.getElementById("news-list");
+
+newsData.forEach(item => {
+  const li = document.createElement("li");
+  li.className = "news-item";
+  li.innerHTML = `<a href="${item.url}">${item.date}｜${item.title}</a>`;
+  list.appendChild(li);
+});
 
