4778f73b378247f5c227795a54fa25733853b874
taisei0828 <o.taisei.0828@gmail.com>
2026-01-31 13:46:09 +0900

    Update news.js

--- news.js
+++ news.js
@@ -1,30 +1,18 @@
-const newsData = [
-  {
-    date: "2026.1.26",
-    category: "ムービー",
-    title: "嵐よりファンクラブ会員の皆さまへ 嬉しいお知らせ",
-    url: "#"
-  },
-  {
-    date: "2026.1.25",
-    category: "ムービー",
-    title: "嵐Movie 1.25 更新！",
-    url: "#"
-  }
-];
+fetch("news.json")
+  .then(res => res.json())
+  .then(data => {
+    const list = document.getElementById("news-list");
+    if (!list) return;
 
-const list = document.getElementById("news-list");
-
-if(list){
-  newsData.forEach(item => {
-    const li = document.createElement("li");
-    li.className = "news-item";
-    li.innerHTML = `
-      <a href="${item.url}">
-        <strong>${item.date}｜${item.category}</strong><br>
-        ${item.title}
-      </a>
-    `;
-    list.appendChild(li);
+    data.forEach(item => {
+      const div = document.createElement("div");
+      div.className = "news-item";
+      div.innerHTML = `
+        <a href="${item.url}">
+          ${item.date}｜${item.category}<br>
+          ${item.title}
+        </a>
+      `;
+      list.appendChild(div);
+    });
   });
-}
